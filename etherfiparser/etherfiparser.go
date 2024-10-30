9f2a53b80fcba4f47e98e15c8126a649d054b9a6
luofj8 <13700243393@139.com>
2024-10-30 18:57:55 +0800

    添加解析合约内部方法

--- etherfiparser/etherfiparser.go
+++ etherfiparser/etherfiparser.go
@@ -3,9 +3,11 @@ package etherfiparser
 import (
 	"context"
 	"etherfi/cache"
+	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
 	"log"
 	"math/big"
 	"sync"
@@ -30,14 +32,16 @@ type ProtocolParser interface {
 }
 
 type EtherFiParser struct {
-	client *ethclient.Client
-	cache  cache.Cache
+	client    *ethclient.Client
+	rpcClient *rpc.Client
+	cache     cache.Cache
 }
 
-func NewEtherFiParser(client *ethclient.Client, cache cache.Cache) *EtherFiParser {
+func NewEtherFiParser(client *ethclient.Client, rpcClient *rpc.Client, cache cache.Cache) *EtherFiParser {
 	return &EtherFiParser{
-		client: client,
-		cache:  cache,
+		client:    client,
+		rpcClient: rpcClient,
+		cache:     cache,
 	}
 }
 
@@ -50,7 +54,7 @@ func (e *EtherFiParser) GetLatestBlock() (uint64, error) {
 	return latestBlock.NumberU64(), nil
 }
 
-// 解析区块并返回指定用户的资产信息
+// ParseBlock 解析区块并返回指定用户的资产信息
 func (e *EtherFiParser) ParseBlock(chain, protocol string, blockNumber uint64, txIndex uint) (bool, error) {
 	block, err := e.client.BlockByNumber(context.Background(), big.NewInt(int64(blockNumber)))
 	if err != nil {
@@ -67,9 +71,17 @@ func (e *EtherFiParser) ParseBlock(chain, protocol string, blockNumber uint64, t
 			defer wg.Done()
 			etherFiContract := common.HexToAddress("0xYourEtherFiContractAddress")
 			if isEtherFiTransaction(tx.To(), etherFiContract) {
-				assetType := getAssetType(tx.Data())
-				symbol, amount, decimals := getAssetDetails(tx)
-				log.Printf(" 类型=%s, symbol=%s, amount=%s, decimals=%d", assetType, symbol, amount.String(), decimals)
+				// 获取合约内部调用的详细信息
+				internalCalls, err := e.traceTransaction(tx.Hash())
+				if err != nil {
+					errChan <- fmt.Errorf("traceTransaction error: %w", err)
+					return
+				}
+
+				for _, call := range internalCalls {
+					symbol, amount, decimals := getAssetDetailsFromCall(call)
+					log.Printf(" 类型=%s, symbol=%s, amount=%s, decimals=%d", call.Type, symbol, amount.String(), decimals)
+				}
 			}
 
 			// 更新解析进度
@@ -100,18 +112,52 @@ func (e *EtherFiParser) ParseBlock(chain, protocol string, blockNumber uint64, t
 }
 
 // 辅助函数：检查是否为 EtherFi 交易
-// 在配置文件中加载合约地址，替代硬编码
 func isEtherFiTransaction(to *common.Address, contractAddress common.Address) bool {
 	return to != nil && *to == contractAddress
 }
 
-// 辅助函数：解析交易类型
-func getAssetType(data []byte) string {
-	// 依据 EtherFi 协议解析资产类型
-	return "Stake" // 示例返回
+// traceTransaction 使用 debug_traceTransaction 解析交易内部调用
+func (e *EtherFiParser) traceTransaction(txHash common.Hash) ([]Call, error) {
+	var result map[string]interface{}
+	err := e.rpcClient.CallContext(context.Background(), &result, "debug_traceTransaction", txHash)
+	if err != nil {
+		return nil, err
+	}
+
+	// 解析内部调用
+	calls, err := parseInternalCalls(result)
+	if err != nil {
+		return nil, err
+	}
+	return calls, nil
 }
 
-// 辅助函数：解析资产详情
-func getAssetDetails(tx *types.Transaction) (string, *big.Int, uint8) {
-	return "ETH", big.NewInt(1000000000000000000), 18 // 示例返回
+// Call 表示合约的内部调用结构
+type Call struct {
+	Type     string   // 调用类型，例如 "transfer"
+	Symbol   string   // 资产符号
+	Amount   *big.Int // 资产数量
+	Decimals int      // 小数位数
+}
+
+// parseInternalCalls 从 traceTransaction 的结果解析出内部调用信息
+func parseInternalCalls(data map[string]interface{}) ([]Call, error) {
+	var calls []Call
+	// 依据 trace 数据结构解析内部调用
+	// 示例代码，这部分需要根据具体 trace 数据结构进行解析
+	for _, item := range data["calls"].([]interface{}) {
+		call := Call{
+			Type:     item.(map[string]interface{})["type"].(string),
+			Symbol:   "ETH",                           // 假设为 ETH，根据实际情况解析
+			Amount:   big.NewInt(1000000000000000000), // 假设金额为 1 ETH
+			Decimals: 18,
+		}
+		calls = append(calls, call)
+	}
+	return calls, nil
+}
+
+// getAssetDetailsFromCall 从内部调用获取资产信息
+func getAssetDetailsFromCall(call Call) (string, *big.Int, uint8) {
+	return call.Symbol, call.Amount, uint8(call.Decimals)
 }
